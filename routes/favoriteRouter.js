2ea67653a1481fe1058fb5bf49927b6f8b499ecb
Prashant Jain <prashantjain0501@gmail.com>
2020-07-31 19:38:18 +0530

    Integrating Client and Server

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -90,6 +90,30 @@ favoriteRouter.route('/')
 
 favoriteRouter.route('/:dishId')
 .options(cors.corsWithOptions, (req,res)=>{res.sendStatus(200); })
+.get(cors.cors, authenticate.verifyUser, (req,res,next)=>{
+    Favorites.findOne({user: req.user._id})
+    .then((favorites) => {
+        if (!favorites) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            return res.json({"exists": false, "favorites": favorites});
+        }
+        else {
+            if (favorites.dishes.indexOf(req.params.dishId) < 0) {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                return res.json({"exists": false, "favorites": favorites});
+            }
+            else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                return res.json({"exists": true, "favorites": favorites});
+            }
+        }
+
+    }, (err) => next(err))
+    .catch((err) => next(err))
+})
 .post(cors.corsWithOptions,authenticate.verifyUser,(req,res,next)=>{
     Favorites.findOne({user: req.user._id})
     .then((favorites) => {
